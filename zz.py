d19018f9f9828ae603c24f0ba15c21ea5a31561c
lanhao34 <lanhao34@gmail.com>
2013-11-29 19:47:13 +0800

    delete a re module test file zz.py

--- zz.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: cp936 -*-
-import re
-text=r'【<span class="keyword">TSDM</span>字幕组】[<span class="keyword">黑魔女学园</span>][Kuromajo_san_ga_Touru!!][13][<span class="keyword">720P</span>][GB简体][MP4]'
-print re.sub(r'(?=\<).*?(?<=>)','', text) 
